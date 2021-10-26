e6ba2a0161b06e538d9cdc9ebdd44631303f074c
andykolbmsu <77354320+andykolbmsu@users.noreply.github.com>
2021-10-25 22:49:44 -0500

    Bootstrap, CSS, and JS Lab

---
+++ Bootstrap-CSS-JS-Lab/scripts.js
@@ -0,0 +1,7 @@
+//    Andy Kolb
+//    Bootstrap, CSS, and JS Lab
+//    scripts.js
+//    10/25/2021
+
+// document.write("<footer class='text-light bg-dark'>Thank you!</footer>");
+document.getElementById("theFooter").innerHTML = "<footer class='text-light bg-dark'>Thank you!</footer>";
